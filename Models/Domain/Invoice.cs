c035aa1cf95d67475d31e71813c2c72f1809f05d
gathonimburu5 <gathonimburu5@gmail.com>
2023-04-26 17:05:26 +0300

    More update on 26/04/2023

--- Models/Domain/Invoice.cs
+++ Models/Domain/Invoice.cs
@@ -4,15 +4,19 @@ using NuGet.Packaging.Signing;
 
 namespace EmployeeClient.Models.Domain
 {
-    public class Invoice
-    {
-        public int InvoiceId { get; set; }
-        public DateTimeOffset InvoiceDate { get; set; } = DateTimeOffset.UtcNow;
-        public decimal Total { get; set; }
-        [ForeignKey("Customer")]
-        public int CustomerId { get; set; }
-        public virtual Customer? Customer { get; set; }
-        public string InvoiceCode { get; set; } = string.Empty;
-        public virtual List<InvoiceDetail> InvoiceDetails { get; set; } = new List<InvoiceDetail>();
-    }
+	public class Invoice
+	{
+		public int InvoiceId { get; set; }
+		[Display(Name = "Invoice Date")]
+		public DateTimeOffset InvoiceDate { get; set; } = DateTimeOffset.UtcNow;
+		[Display(Name = "Invoice Date Due")]
+		public DateTimeOffset InvoiceDateDue { get; set; } = DateTimeOffset.UtcNow.AddDays(7);
+		public decimal Total { get; set; }
+		[ForeignKey("Customer")][Display(Name = "Customer Name")]
+		public int CustomerId { get; set; }
+		public virtual Customer? Customer { get; set; }
+		[Display(Name = "Invoice Code")]
+		public string InvoiceCode { get; set; } = string.Empty;
+		public virtual List<InvoiceDetail> InvoiceDetails { get; set; } = new List<InvoiceDetail>();
+	}
 }
--- Services/Implementation/InvoiceService.cs
+++ Services/Implementation/InvoiceService.cs
@@ -5,121 +5,126 @@ using System.Text;
 
 namespace EmployeeClient.Services.Implementation
 {
-    public class InvoiceService : IInvoiceService
-    {
-        private string url = "https://localhost:7243/api/Invoice";
-        private HttpClient client = new HttpClient();
-        public Invoice CreateInvoice(Invoice invoice)
-        {
-            string json = JsonConvert.SerializeObject(invoice);
-            HttpResponseMessage responseMessage = client.PostAsync(url, new StringContent(json, Encoding.UTF8, "application/json")).Result;
-            if (responseMessage.IsSuccessStatusCode)
-            {
-                string content = responseMessage.Content.ReadAsStringAsync().Result;
-                var result = JsonConvert.DeserializeObject<Invoice>(content);
-                if (result != null) invoice = result;
-            }
-            else
-            {
-                string response = responseMessage.Content.ReadAsStringAsync().Result;
-                throw new Exception("Error at the API EndPoint. " + response);
-            }
-            return invoice;
-        }
+	public class InvoiceService : IInvoiceService
+	{
+		private string url = "https://localhost:7243/api/Invoice";
+		private HttpClient client = new HttpClient();
+		public Invoice CreateInvoice(Invoice invoice)
+		{
+			invoice.InvoiceDetails.RemoveAll(x => x.IsDeleted == true);
+			
+			string json = JsonConvert.SerializeObject(invoice);
+			HttpResponseMessage responseMessage = client.PostAsync(url, new StringContent(json, Encoding.UTF8, "application/json")).Result;
+			if (responseMessage.IsSuccessStatusCode)
+			{
+				string content = responseMessage.Content.ReadAsStringAsync().Result;
+				var result = JsonConvert.DeserializeObject<Invoice>(content);
+				if (result != null) invoice = result;
+			}
+			else
+			{
+				string response = responseMessage.Content.ReadAsStringAsync().Result;
+				throw new Exception("Error at the API EndPoint. " + response);
+			}
+			return invoice;
+		}
 
-        public bool DeleteInvoice(int id)
-        {
-            Invoice invoice = new Invoice();
-            url = url + "/" + id;
-            HttpResponseMessage responseMessage = client.DeleteAsync(url).Result;
-            if (!responseMessage.IsSuccessStatusCode)
-            {
-                string result = responseMessage.Content.ReadAsStringAsync().Result;
-                throw new Exception("Error at the API EndPoint" + result);
-            }
-            return true;
-        }
+		public bool DeleteInvoice(int id)
+		{
+			Invoice invoice = new Invoice();
+			url = url + "/" + id;
+			HttpResponseMessage responseMessage = client.DeleteAsync(url).Result;
+			if (!responseMessage.IsSuccessStatusCode)
+			{
+				string result = responseMessage.Content.ReadAsStringAsync().Result;
+				throw new Exception("Error at the API EndPoint" + result);
+			}
+			return true;
+		}
 
-        public List<Invoice> GetAllInvoices()
-        {
-            List<Invoice> invoices = new List<Invoice>();
-            try
-            {
-                HttpResponseMessage responseMessage = client.GetAsync(url).Result;
-                if (responseMessage.IsSuccessStatusCode)
-                {
-                    string results = responseMessage.Content.ReadAsStringAsync().Result;
-                    var items = JsonConvert.DeserializeObject<List<Invoice>>(results);
-                    if (items != null) invoices = items;
-                }
-                else
-                {
-                    string repo = responseMessage.Content.ReadAsStringAsync().Result;
-                    throw new Exception("Error at the API EndPoint" + repo);
-                }
-            }
-            catch (Exception ex)
-            {
-                throw new Exception("Error at the API EndPoint" + ex.Message);
-            }
-            return invoices;
-        }
+		public List<Invoice> GetAllInvoices()
+		{
+			List<Invoice> invoices = new List<Invoice>();
+			try
+			{
+				HttpResponseMessage responseMessage = client.GetAsync(url).Result;
+				if (responseMessage.IsSuccessStatusCode)
+				{
+					string results = responseMessage.Content.ReadAsStringAsync().Result;
+					var items = JsonConvert.DeserializeObject<List<Invoice>>(results);
+					if (items != null) invoices = items;
+				}
+				else
+				{
+					string repo = responseMessage.Content.ReadAsStringAsync().Result;
+					throw new Exception("Error at the API EndPoint" + repo);
+				}
+			}
+			catch (Exception ex)
+			{
+				throw new Exception("Error at the API EndPoint" + ex.Message);
+			}
+			return invoices;
+		}
 
-        public Invoice GetInvoiceById(int id)
-        {
-            Invoice invoice = new Invoice();
-            url = url + "/" + id;
-            try
-            {
-                HttpResponseMessage responseMessage = client.GetAsync(url).Result;
-                if (responseMessage.IsSuccessStatusCode)
-                {
-                    string result = responseMessage.Content.ReadAsStringAsync().Result;
-                    var item = JsonConvert.DeserializeObject<Invoice>(result);
-                    if (item != null) invoice = item;
-                }
-                else
-                {
-                    string result = responseMessage.Content.ReadAsStringAsync().Result;
-                    throw new Exception("Error at the API EndPoint" + result);
-                }
-            }
-            catch (Exception ex)
-            {
-                throw new Exception("Error at the API EndPoint" + ex.Message);
-            }
-            return invoice;
-        }
+		public Invoice GetInvoiceById(int id)
+		{
+			Invoice invoice = new Invoice();
+			url = url + "/" + id;
+			try
+			{
+				HttpResponseMessage responseMessage = client.GetAsync(url).Result;
+				if (responseMessage.IsSuccessStatusCode)
+				{
+					string result = responseMessage.Content.ReadAsStringAsync().Result;
+					var item = JsonConvert.DeserializeObject<Invoice>(result);
+					if (item != null) invoice = item;
+				}
+				else
+				{
+					string result = responseMessage.Content.ReadAsStringAsync().Result;
+					throw new Exception("Error at the API EndPoint" + result);
+				}
+			}
+			catch (Exception ex)
+			{
+				throw new Exception("Error at the API EndPoint" + ex.Message);
+			}
+			return invoice;
+		}
 
-        public string InvoiceCodes()
-        {
-            string code = "";
-            var invoiceCodes = GetAllInvoices().Max(x => x.InvoiceCode);
-            if (invoiceCodes == null)
-            {
-                code = "IN/0001/" + DateTime.Now.Year;
-            }
-            else
-            {
-                int lastDigit = 1;
-                int.TryParse(invoiceCodes.Substring(3, 4), out lastDigit);
-                code = $"IN/{(lastDigit + 1).ToString().PadLeft(4, '0')}/{DateTime.Now.Year}";
-            }
-            return code;
-        }
+		public string InvoiceCodes()
+		{
+			string code = "";
+			var invoiceCodes = GetAllInvoices().Max(x => x.InvoiceCode);
+			if (invoiceCodes == null)
+			{
+				code = "IN/0001/" + DateTime.Now.Year;
+			}
+			else
+			{
+				int lastDigit = 1;
+				int.TryParse(invoiceCodes.Substring(3, 4), out lastDigit);
+				code = $"IN/{(lastDigit + 1).ToString().PadLeft(4, '0')}/{DateTime.Now.Year}";
+			}
+			return code;
+		}
 
-        public Invoice UpdateInvoice(Invoice invoice)
-        {
-            int id = invoice.InvoiceId;
-            url = url + "/" + id;
-            string json = JsonConvert.SerializeObject(invoice);
-            HttpResponseMessage responseMessage = client.PutAsync(url, new StringContent(json, Encoding.UTF8, "application/json")).Result;
-            if (!responseMessage.IsSuccessStatusCode)
-            {
-                string result = responseMessage.Content.ReadAsStringAsync().Result;
-                throw new Exception("Error at the API EndPoint" + result);
-            }
-            return invoice;
-        }
-    }
+		public Invoice UpdateInvoice(Invoice invoice)
+		{
+			int id = invoice.InvoiceId;
+			url = url + "/" + id; 
+			
+			invoice.InvoiceDetails.RemoveAll(x => x.IsDeleted == true);
+			
+			string json = JsonConvert.SerializeObject(invoice);
+			HttpResponseMessage responseMessage = client.PutAsync(url, new StringContent(json, Encoding.UTF8, "application/json")).Result;
+			if (!responseMessage.IsSuccessStatusCode)
+			{
+				string result = responseMessage.Content.ReadAsStringAsync().Result;
+				throw new Exception("Error at the API EndPoint" + result);
+			}
+			return invoice;
+		}
+	}
 }
